42c5c63a38a30e143dcec2532d38ef61ce362464
unknown <mahdi.soleimani70@gmail.com>
2025-05-28 01:08:40 -0700

    Add Text Input with showing in the field sync

--- App.js
+++ App.js
@@ -1,53 +1,76 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 // Class Base Implement
-class App extends React.Component {
-  state = {
-    firstName : "یونس",
-  }
-
-  handleNameChange = () => {
-    this.setState({
-      firstName: "مهدی",
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.header_text}>Hi React Native</Text>
-        </View>
-        <View>
-          <Text style={styles.body}>سلام {this.state.firstName} دوستان عزیز</Text>
-          <Button title="تغییر" onPress={this.handleNameChange}/>
-        </View>
-      </View>
-    )
-  }
-}
-
-// Function Base Implement
-// const App = () => {
-//   const [firstName, setFirstName] = useState('یونس');
-
-//   const handleNameChange = () => {
-//     setFirstName("مهدی")
+// class App extends React.Component {
+//   state = {
+//     firstName : "یونس",
 //   }
 
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.header}>
-//         <Text style={styles.header_text}>Hi React Native</Text>
+//   handleNameChange = () => {
+//     this.setState({
+//       firstName: "مهدی",
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.header}>
+//           <Text style={styles.header_text}>Hi React Native</Text>
+//         </View>
+//         <View>
+//           <Text style={styles.body}>سلام {this.state.firstName} دوستان عزیز</Text>
+//           <Button title="تغییر" onPress={this.handleNameChange}/>
+//         </View>
 //       </View>
-//       <View>
-//         <Text style={styles.body}>سلام {firstName} دوستان عزیز</Text>
-//         <Button title="تغییر" onPress={handleNameChange}/>
-//       </View>
-//     </View>
-//   )
+//     )
+//   }
 // }
+
+// Function Base Implement
+const App = () => {
+
+  const [firstName, setFirstName] = useState('یونس');
+  const [fullName, setFullName] = useState();
+  const [age, setAge] = useState();
+
+  const handleNameChange = () => {
+    setFirstName("مهدی")
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Title */}
+      <View style={styles.header}>
+        <Text style={styles.header_text}>Hi React Native</Text>
+      </View>
+
+      {/* Button */}
+      <View>
+        <Text style={styles.body}>سلام {firstName} دوستان عزیز</Text>
+        <Button title="تغییر" onPress={handleNameChange}/>
+      </View>
+      {/* Inputs */}
+      <View style = {{ 
+        marginBottom:30,
+        backgroundColor: "orange",
+        padding: 20,
+        width: 300,
+        borderRadius: 10,
+       }} >
+        <Text style={styles.label}>نام و نام خانوادگی: {fullName} </Text>
+        <Text style={styles.label}>سن: {age} </Text>
+      </View>
+      <View>
+        <Text style={styles.label}>نام و نام خانوادگی: {fullName} </Text>
+        <TextInput style={styles.text_input} placeholder="مثلا امیرعلی" onChangeText={(value) => setFullName(value) } />
+        <Text style={styles.label}>سن: {age} </Text>
+        <TextInput style={styles.text_input} keyboardType="numeric" placeholder="مثلا 25" onChangeText={(value) => setAge(value) }/>
+      </View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -69,6 +92,20 @@ const styles = StyleSheet.create({
     margin: 20,
     fontSize: 20, 
     fontWeight: "bold",
+  },
+  label: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  text_input: {
+    borderWidth: 1,
+    borderColor: "lightGrey",
+    borderRadius: 10,
+    padding: 5,
+    margin: 10,
+    width: 250,
+    textAlign: "center",
+    fontSize: 15,
   }
 })
 
